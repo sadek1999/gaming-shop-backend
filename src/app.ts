ff736483a7067349e84c42eaafe70dad2e926152
sadek1999 <sadekali1530@gmail.com>
2024-09-14 23:27:19 +0600

    basic seup

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import express from 'express'
-const app = express()
-const port = 3000
+ export const app = express()
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
